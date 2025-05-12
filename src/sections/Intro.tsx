a08439676a6baaab31d1301ea75e698a96feb39e
Olasubomi Elijah Olubisi <123889642+firstaxel@users.noreply.github.com>
2025-05-12 13:48:42 +0100

    Update Intro.tsx

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -5,7 +5,7 @@ import { type FC } from "react";
 
 const Intro: FC = () => {
   return (
-    <section className="section h-full md:h-screen ">
+    <section className="section h-full md:h-screen " id="intro">
       <div className="container h-full flex flex-col items-center justify-center ">
         <TextAnimation delay={0.5} animateOnScroll={true}>
           <h2 className="text-4xl md:text-7xl lg:w-[90%] lg:text-8xl">
