691f157c497ad136101f58757f7aeef588de43a0
firstaxel <devtwinkle8@gmail.com>
2025-05-29 21:58:10 +0100

    adding cms

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,12 +1,11 @@
-"use client";
+'use client'
 
-import Button from "@/components/Button";
-import { ContainerTextFlip } from "@/components/container-text-flip";
-import { InfiniteSlider } from "@/components/infinite-slider";
-import RevealText from "@/components/reveal-text";
-import { heroImages } from "@/lib/constants";
-import Link from "next/link";
-import { FC } from "react";
+import Button from '@/components/Button'
+import { ContainerTextFlip } from '@/components/container-text-flip'
+import { InfiniteSlider } from '@/components/infinite-slider'
+import { heroImages } from '@/lib/constants'
+import Link from 'next/link'
+import { FC } from 'react'
 
 const Hero: FC = () => {
   return (
@@ -14,13 +13,11 @@ const Hero: FC = () => {
       <div className="h-full py-[100px] space-y-4 flex flex-col" id="home">
         <div className="container space-y-4 not-first-of-type:items-start h-full">
           <div>
-            <p className="text-xl font-bold">
-              Are you ready to scale your business?
-            </p>
+            <p className="text-xl font-bold">Are you ready to scale your business?</p>
             <h1 className="text-6xl lg:text-8xl flex flex-col md:flex-row items-start md:items-center gap-4">
-              Dashhub builds{" "}
+              Dashhub builds{' '}
               <ContainerTextFlip
-                words={["startups", "ventures", "innovations"]}
+                words={['startups', 'ventures', 'innovations']}
                 animationDuration={1000}
                 interval={5000}
               />
@@ -28,15 +25,10 @@ const Hero: FC = () => {
           </div>
 
           <Link href="https://calendly.com/dashhubtech/audit-call">
-            <Button
-              className="w-[200px] flex items-center justify-center"
-              variant="secondary"
-            >
-
-             Let&apos;s get started
+            <Button className="w-[200px] flex items-center justify-center" variant="secondary">
+              Let&apos;s get started
             </Button>
-
-            </Link>
+          </Link>
         </div>
         <div>
           <InfiniteSlider speedOnHover={1} gap={24}>
@@ -53,7 +45,7 @@ const Hero: FC = () => {
         {/* <RevealText /> */}
       </div>
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
